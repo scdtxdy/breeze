abe149623bd611122e9a282d2543e369dd5abc9a
scd0326 <13941369466@163.com>
2022-02-09 23:34:33 +0800

    创建common-core模块

---
+++ breeze-common/breeze-common-core/src/main/java/com/example/breeze/common/core/BreezeCommonCoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.breeze.common.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BreezeCommonCoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BreezeCommonCoreApplication.class, args);
+    }
+
+}
---
+++ breeze-common/breeze-common-core/src/test/java/com/example/breeze/common/core/BreezeCommonCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.breeze.common.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BreezeCommonCoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
